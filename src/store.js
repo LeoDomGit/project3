aebbd65b8a0f3808cb165d16f490e0b31183e3d8
LeoDomGit <leodomsolar@gmail.com>
2023-10-02 18:31:32 +0700

    C

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import majorReducer from "./redux/MajorSlice";
-export const store = configureStore({
-    reducer:{
-        majors:majorReducer
-    }
-});
